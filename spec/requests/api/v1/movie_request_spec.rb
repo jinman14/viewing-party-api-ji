549386fb6f6689573752d6efce4a2f5b07395f88
Jacob Inman <jrinman6295@gmail.com>
2025-03-30 22:13:31 -0700

    second endpoint happy paths reworked with webmock

--- spec/requests/api/v1/movie_request_spec.rb
+++ spec/requests/api/v1/movie_request_spec.rb
@@ -25,6 +25,30 @@ RSpec.describe "Movies API Endpoints", type: :request do
       expect(json[:data].first[:attributes][:title]).to eq("The Shawshank Redemption")
       expect(json[:data].first[:attributes][:vote_average]).to eq(8.709)
     end
+
+    it "can retrieve a list of movies including a searched term" do
+      json_response = File.read('spec/fixtures/movie_including_lord_of_the_query.json')
+
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?language=en-US&query=Lord%20of%20the").
+        with(
+          headers: {
+          'Accept'=>'application/json',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Authorization'=>'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDJiMTMyZWYzMmNlM2Q4ZTJjNzIyN2E0OGQxNmIyZSIsIm5iZiI6MTc0Mjk0NTQ4Ni4zMjUsInN1YiI6IjY3ZTMzY2NlN2I3MzEzYjVhZWYwOGViNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KtcPqZylsno9_5uN7K2SzsgnTFV_guyMQwpX3bZKtp8',
+          'User-Agent'=>'Faraday v2.10.1'
+          }).
+      to_return(status: 200, body: json_response, headers: {})
+
+      get "/api/v1/movies?query=Lord of the"
+
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data].first[:id]).to eq(123)
+      expect(json[:data].first[:type]).to eq("movie")
+      expect(json[:data].first[:attributes][:title]).to eq("The Lord of the Rings")
+      expect(json[:data].first[:attributes][:vote_average]).to eq(6.6)
+    end
   end
 end
 # describe "Show Movie Endpoint with details" do
